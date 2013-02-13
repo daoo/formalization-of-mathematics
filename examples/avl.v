7eb15e7ab6d62abe4001cf2fb4d8d94e4d223207
Daniel Oom <daoo314@gmail.com>
2013-02-13 14:14:14 +0100

    Work on AVL trees

--- examples/avl.v
+++ examples/avl.v
@@ -1,26 +1,26 @@
-Require Import Arith ZArith Orders.
+Require Import Arith ZArith.
 
-Section bin_tree.
-  Inductive bin_tree (X: Type) : Type :=
-    | bt_nil: bin_tree X
-    | bt_node: X -> bin_tree X -> bin_tree X -> bin_tree X.
+Section Tree.
+  Inductive tree (X: Type) : Type :=
+    | Leaf: tree X
+    | Node: X -> tree X -> tree X -> tree X.
 
-  Implicit Arguments bt_nil [[X]].
-  Implicit Arguments bt_node [[X]].
+  Implicit Arguments Leaf [[X]].
+  Implicit Arguments Node [[X]].
 
-  Fixpoint node_count {X: Type} (t: bin_tree X) : nat :=
+  Fixpoint node_count {X: Type} (t: tree X) : nat :=
     match t with
-    | bt_nil        => 0
-    | bt_node _ a b => 1 + node_count a + node_count b
+    | Leaf       => 0
+    | Node _ a b => 1 + node_count a + node_count b
     end.
 
-  Fixpoint tree_height {X: Type} (t: bin_tree X) : nat :=
+  Fixpoint tree_height {X: Type} (t: tree X) : nat :=
     match t with
-    | bt_nil        => 0
-    | bt_node _ a b => 1 + max (tree_height a) (tree_height b)
+    | Leaf       => 0
+    | Node _ a b => 1 + max (tree_height a) (tree_height b)
   end.
 
-  Theorem bin_tree_height_le_count: forall X: Type, forall t: bin_tree X,
+  Theorem tree_height_le_count: forall X: Type, forall t: tree X,
     tree_height t <= node_count t.
   Proof.
     intros X t. induction t as [| x a IHa b].
@@ -32,31 +32,49 @@ Section bin_tree.
         intro H. rewrite IHb. apply le_plus_r.
   Qed.
 
-  Fixpoint search {X: Compare} (x: X) (t: bin_tree X) : option X :=
+  Fixpoint search {X: Type} (x: X) (t: tree X) : option X :=
     match t with
-    | bt_nil        => None
-    | bt_node y a b => if x < y then search a
-                  else if x > y then search b
-                     else y
+    | Leaf       => None
+    | Node y a b => match comp x y with
+      | LT => search x a
+      | EQ => Some y
+      | GT => search x b
+    end
   end.
 
-  Section avl.
+  Fixpoint insert (x: X) (t: tree X) : tree X :=
+    match t with
+    | Leaf       => Node x Leaf Leaf
+    | Node y a b => match comp x y with
+      | LT => Node y (insert x a) b
+      | EQ => Node x a b
+      | GT => Node y a (insert x b)
+    end
+  end.
+
+  Theorem insert_search: forall (x: X), forall (t: tree X),
+    search x (insert x t) = Some x.
+  Proof.
+    intros x t. induction t as [| y a IHa b].
+      simpl. admit.
+
+  Section AVL.
     Open Scope Z_scope.
 
-    Definition balance_factor {X: Type} (t: bin_tree X) : Z :=
+    Definition balance_factor {X: Type} (t: tree X) : Z :=
       match t with
-      | bt_nil        => 0
-      | bt_node _ a b => Z.of_nat (tree_height a) - Z.of_nat (tree_height b)
+      | Leaf       => 0
+      | Node a _ b => Z.of_nat (tree_height a) - Z.of_nat (tree_height b)
       end.
 
-    Definition is_balanced {X: Type} (t: bin_tree X) : Prop :=
+    Definition is_balanced {X: Type} (t: tree X) : Prop :=
       -1 <= balance_factor t <= 1.
 
-    Inductive AVL {X: Type} : bin_tree X -> Prop :=
-      | avl_nil: AVL bt_nil
+    Inductive AVL {X: Type} : tree X -> Prop :=
+      | avl_nil: AVL Leaf
       | avl_node: forall n, is_balanced n -> AVL n.
 
-    Fixpoint avl_insert {X: Type} (x: X) (t: bin_tree X) : bin_tree X :=
+    Fixpoint avl_insert {X: Type} (x: X) (t: tree X) : tree X :=
       match
-  End avl.
-End bin_tree.
+  End AVL.
+End Tree.
