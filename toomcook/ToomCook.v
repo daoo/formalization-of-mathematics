9e1608fb16b629e44e06f9aa7f0b239233d7b781
Daniel Oom <daoo314@gmail.com>
2013-04-10 10:10:29 +0200

    Rewrite split to match haskell implementation

--- toomcook/ToomCook.v
+++ toomcook/ToomCook.v
@@ -15,9 +15,9 @@ Variable R : comRingType.
 Definition base_exponent (k: nat) (n m: {poly R}) : nat :=
   maxn (divn (size n) k) (divn (size m) k) .+1.
 
-Fixpoint split_poly (k: nat) (n: {poly R}) :=
+Fixpoint split (k: nat) (n: {poly R}) : seq {poly R} :=
   match k with
-  | O    => 0
-  | S k' => rmodp n 'X^k' + split_poly k' (rdivp n 'X^k')
+  | O    => nil
+  | S k' => rmodp n 'X^k' :: split k' (rdivp n 'X^k')
   end.
 End toomCook.
