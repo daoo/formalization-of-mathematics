3034cef995758994f9a364757dd153b0a891de8d
Daniel Oom <daoo314@gmail.com>
2013-04-26 10:36:20 +0200

    Rewrite

--- toomcook/ToomCook.v
+++ toomcook/ToomCook.v
@@ -1,5 +1,6 @@
 Require Import ssreflect ssrfun ssrbool eqtype ssrnat div seq matrix.
 Require Import path choice fintype tuple finset ssralg poly polydiv bigop.
+Require Import mxpoly.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -10,50 +11,49 @@ Open Scope ring_scope.
 
 Section toomCook.
 
-Variable R : comRingType.
-Variable m : nat. (* Number of splits in Toom-m *)
-Definition np : nat := m.*2 .-1. (* Number of evaluation points *)
-Variable evaluation_mat : 'M[{poly R}]_(np, m).
-Variable interpolation_mat : 'M[{poly R}]_np.
+Variable R : comUnitRingType.
 Implicit Types p q : {poly R}.
 
-Definition exponent (k: nat) p q : nat :=
-  maxn (divn (size p) k) (divn (size q) k) .+1.
+Variable number_points m : nat.
+Definition d : nat := number_points.-1.
+Variable inter_points : 'cV[R]_(d + 1).
 
-(*
- * m is a k by 1 matrix
- * m(0, 0) = (p / x^0b) % x^b
- * m(1, 0) = (p / x^1b) % x^b
- * m(2, 0) = (p / x^2b) % x^b
- * m(3, 0) = (p / x^3b) % x^b
- * ...
-*)
+Definition V_e : 'M[{poly R}]_(d+1, m) :=
+  \matrix_(i < (d + 1), j < m) ((inter_points i ord0) %:P)^+j.
 
-Definition split (n: nat) p : 'cV[{poly R}]_m :=
-  \col_i rmodp (rdivp p 'X^(i * n)) 'X^n.
+Definition V_I : 'M[R]_(d + 1) :=
+  \matrix_(i < d + 1, j < d + 1) ((inter_points i ord0))^+j.
 
-Definition evaluate (vec: 'cV[{poly R}]_m) : 'cV[{poly R}]_np :=
-  (* TODO: vec must have correct order, in the haskell implementation we reverse the vector (list) *)
-  evaluation_mat *m vec.
+Definition V_I_inv : 'M[{poly R}]_(d + 1) :=
+  \matrix_(i < d + 1, j < d + 1) ((invmx V_I) i j) %:P.
 
-Definition interpolate (vec: 'cV[{poly R}]_np) : 'cV[{poly R}]_np :=
-  interpolation_mat *m vec.
+Definition exponent (m: nat) p q : nat :=
+  maxn (divn (size p) m) (divn (size q) m) .+1.
 
-Definition recompose (n: nat) (vec: 'cV[{poly R}]_np) : {poly R} :=
-  ((\row_i 'X^(i * n)) *m vec) ord0 ord0.
+Definition split (n b: nat) p : {poly {poly R}} :=
+  \poly_(i < n) rmodp (rdivp p 'X^(i * b)) 'X^b.
+
+Definition evaluate (u : {poly {poly R}}) : 'cV[{poly R}]_(d + 1) :=
+  V_e *m (poly_rV u)^T.
+
+Definition interpolate (u : 'cV[{poly R}]_(d + 1)) : {poly {poly  :=
+  rVpoly (V_I_inv *m u)^T.
+
+Definition recompose (b: nat) (w: {poly {poly R}}) : {poly R} :=
+  w.['X^b].
 
 Fixpoint toom_cook_rec (n: nat) p q : {poly R} :=
   match n with
   | 0%N   => p * q
-  | n'.+1 =>
-    let e   := exponent m p q in
-    let p'  := split e p in
-    let q'  := split e q in
-    let p'' := evaluate p' in
-    let q'' := evaluate q' in
-    let r   := \col_i (toom_cook_rec n' (p'' i ord0) (q'' i ord0)) in
-    let r'  := interpolate r
-     in recompose e r'
+  | n'.+1 => if (size p <= 2) || (size q <= 2) then p * q else
+    let b  := exponent m p q in
+    let u  := split m b p in
+    let v  := split m b q in
+    let u' := evaluate u in
+    let v' := evaluate v in
+    let r  := \col_i toom_cook_rec n' (u' i ord0) (v' i ord0) in
+    let r' := interpolate r
+     in recompose b r'
   end.
 
 Definition toom_cook p q : {poly R} :=
