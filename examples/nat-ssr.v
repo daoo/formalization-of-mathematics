e24875af82e9f44faa7cc48302665e971bfc51a7
Daniel Oom <daoo314@gmail.com>
2013-05-21 15:00:51 +0200

    More exempel

--- examples/nat-ssr.v
+++ examples/nat-ssr.v
@@ -10,6 +10,12 @@ Fixpoint plus (n m: nat) :=
   | S n' => S (plus n' m)
   end.
 
+Fixpoint mult (n m: nat) :=
+  match n with
+  | O => O
+  | S n' => plus m (mult n' m)
+  end.
+
 Theorem plus0n: forall (n: nat),
   plus O n = n.
 Proof.
@@ -44,3 +50,61 @@ Proof.
   elim=> [ // | n' IH m o ] /=.
     by rewrite IH.
 Qed.
+
+
+Theorem mult0n: forall (n: nat),
+  mult O n = O.
+Proof.
+  done.
+Qed.
+
+Theorem multn0: forall (n: nat),
+  mult n O = O.
+Proof.
+  elim=> [ // | n' IH ] /=.
+    by rewrite IH.
+Qed.
+
+Theorem mult1n: forall (n: nat),
+  mult (S O) n = n.
+Proof.
+  by apply plusn0.
+Qed.
+
+Theorem multn1: forall (n: nat),
+  mult n (S O) = n.
+Proof.
+  elim=> [ // | n' IH ] /=.
+    by rewrite IH.
+Qed.
+
+Theorem plusSwap: forall (n m o: nat),
+  plus n (plus m o) = plus m (plus n o).
+Proof.
+  move=> n m o.
+  by rewrite plusA [X in plus X]plusC -plusA.
+Qed.
+
+Theorem multSn: forall (n m: nat),
+  mult m (S n) = plus m (mult m n).
+Proof.
+  move=> n m.
+  move: m.
+  elim=> [ // | m' IH ].
+    rewrite /=.
+Qed.
+
+Theorem multC: forall (n m: nat),
+  mult n m = mult m n.
+Proof.
+  induction n as [ | n' IH ].
+    (* n = 0 *)
+    intros m.
+    simpl.
+    rewrite -> multn0.
+    reflexivity.
+
+    (* n = S n' *)
+    intros m.
+    simpl.
+    
--- examples/nat.v
+++ examples/nat.v
@@ -10,6 +10,12 @@ Fixpoint plus (n m: nat) :=
   | S n' => S (plus n' m)
   end.
 
+Fixpoint mult (n m: nat) :=
+  match n with
+  | O => O
+  | S n' => plus m (mult n' m)
+  end.
+
 Theorem plus0n: forall (n: nat),
   plus O n = n.
 Proof.
@@ -71,3 +77,78 @@ Proof.
     rewrite -> IH.
     reflexivity.
 Qed.
+
+Theorem mult0n: forall (n: nat),
+  mult O n = O.
+Proof.
+  reflexivity.
+Qed.
+
+Theorem multn0: forall (n: nat),
+  mult n O = O.
+Proof.
+  induction n as [ | n' IH ].
+    reflexivity.
+    simpl.
+    rewrite -> IH.
+    reflexivity.
+Qed.
+
+Theorem mult1n: forall (n: nat),
+  mult (S O) n = n.
+Proof.
+  intros n.
+  simpl.
+  rewrite -> plusn0.
+  reflexivity.
+Qed.
+
+Theorem multn1: forall (n: nat),
+  mult n (S O) = n.
+Proof.
+  induction n as [ | n' IH ].
+    (* n = 0 *)
+    reflexivity.
+
+    (* n = S n' *)
+    simpl.
+    rewrite -> IH.
+    reflexivity.
+Qed.
+
+Theorem plusSwap: forall (n m o: nat),
+  plus n (plus m o) = plus m (plus n o).
+Proof.
+  intros n m o.
+  rewrite plusC.
+  rewrite <- plusA.  
+
+Theorem multSn: forall (n m: nat),
+  mult m (S n) = plus m (mult m n).
+Proof.
+  intros n m.
+  generalize dependent n.
+  induction m as [ | m' IH ].
+    (* n = 0 *)
+    reflexivity.
+
+    (* n = S n' *)
+    intros n.
+    simpl.
+    rewrite IH.
+    rewrite plusA.
+
+Theorem multC: forall (n m: nat),
+  mult n m = mult m n.
+Proof.
+  induction n as [ | n' IH ].
+    (* n = 0 *)
+    intros m.
+    simpl.
+    rewrite -> multn0.
+    reflexivity.
+
+    (* n = S n' *)
+    intros m.
+    simpl.
+    
