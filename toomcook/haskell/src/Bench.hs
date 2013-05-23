56f4ad2392a610ccd53c426bce5f5fb2088c9bef
Daniel Oom <daoo314@gmail.com>
2013-05-23 08:48:24 +0200

    Larger benchmark

--- toomcook/haskell/src/Bench.hs
+++ toomcook/haskell/src/Bench.hs
@@ -7,4 +7,4 @@ import ToomCook
 main :: IO ()
 main = defaultMain $
   map (\i -> bench (show i) $ whnf (\j -> toomCook toom3 j j) i)
-    [100000, 100100..200000]
+    [184467440737095516169, 194467440737095516169..284467440737095516169]
